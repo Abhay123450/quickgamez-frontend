a936dbfbba1d46dd7642f9c66fed7129f55412de
Abhay123450 <95590546+Abhay123450@users.noreply.github.com>
2025-05-12 11:00:02 +0530

    improved animation and accessibility

--- src/lib/components/guess-the-movie/InputSingle.svelte
+++ src/lib/components/guess-the-movie/InputSingle.svelte
@@ -1,8 +1,6 @@
 <script lang="ts">
-	// export let charObj = {};
-	// let char = charObj.char;
-	// let isGuessed = charObj.isGuessed;
-	// let isGuessedByUser = charObj.isGuessedByUser;
+	import { scale } from 'svelte/transition';
+
 	export let character: string;
 	export let isGuessed: boolean;
 	export let isGuessedByUser: boolean;
@@ -11,28 +9,34 @@
 <div class="flex w-8 h-8 md:w-10 md:h-10 items-center justify-center rounded-md mx-1 my-2 md:mx-2">
 	{#if isGuessed && isGuessedByUser}
 		<input
+			transition:scale={{ duration: 500 }}
 			type="text"
-			class=" w-full h-full text-center text-2xl rounded-md focus:outline-none bg-green-300"
+			class=" w-full h-full text-center text-2xl rounded-md focus:outline-none bg-green-300 opacity-100"
 			value={character}
+			aria-label="{character} is guessed"
 			disabled
 		/>
 	{:else if character === ' '}
 		<input
 			type="text"
 			class=" w-full h-full text-center text-2xl rounded-md focus:outline-none bg-transparent"
+			value=" "
+			aria-label="Space"
 			disabled
 		/>
 	{:else if isGuessed && !isGuessedByUser}
 		<input
 			type="text"
-			class=" w-full h-full text-center text-2xl rounded-md focus:outline-none bg-neutral-100"
+			class=" w-full h-full text-center text-2xl rounded-md focus:outline-none bg-neutral-100 opacity-100"
 			value={character}
+			aria-label="{character}, is guessed"
 			disabled
 		/>
 	{:else}
 		<input
 			type="text"
-			class=" w-full h-full text-center text-2xl rounded-md focus:outline-none bg-white border border-solid border-gray-500"
+			class=" w-full h-full text-center text-2xl rounded-md focus:outline-none bg-white opacity-100 border border-solid border-gray-500"
+			aria-label="Empty, not guessed"
 			disabled
 		/>
 	{/if}
