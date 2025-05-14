4dc181d10e3eb45d478ce623817e176662ae3caf
Abhay123450 <95590546+Abhay123450@users.noreply.github.com>
2025-05-14 20:53:55 +0530

    updated left panel game llinks

--- src/lib/components/Games.svelte
+++ src/lib/components/Games.svelte
@@ -5,15 +5,28 @@
 <div class="flex flex-col w-full h-auto p-1 bg-white rounded-b space-y-1 overflow-y-auto">
 	<a
 		class="flex p-2 w-auto rounded-md hover:bg-neutral-100 font-medium {$page.url.pathname ===
-		'/games/guess-the-movie'
+		'/games/guess-the-movie/hollywood'
 			? 'border border-neutral-400 bg-neutral-100'
 			: ''}"
-		href="/games/guess-the-movie"
-		aria-current={$page.url.pathname === '/games/guess-the-movie'}
+		href="/games/guess-the-movie/hollywood"
+		aria-current={$page.url.pathname === '/games/guess-the-movie/hollywood'}
 		on:click={(e) => {
-			if ($page.url.pathname === '/games/guess-the-movie') {
+			if ($page.url.pathname === '/games/guess-the-movie/hollywood') {
 				return e.preventDefault();
 			}
-		}}>Guess The Movie</a
+		}}>Guess The Movie - Hollywood</a
+	>
+	<a
+		class="flex p-2 w-auto rounded-md hover:bg-neutral-100 font-medium {$page.url.pathname ===
+		'/games/guess-the-movie/bollywood'
+			? 'border border-neutral-400 bg-neutral-100'
+			: ''}"
+		href="/games/guess-the-movie/bollywood"
+		aria-current={$page.url.pathname === '/games/guess-the-movie/bollywood'}
+		on:click={(e) => {
+			if ($page.url.pathname === '/games/guess-the-movie/bollywood') {
+				return e.preventDefault();
+			}
+		}}>Guess The Movie - Bollywood</a
 	>
 </div>
