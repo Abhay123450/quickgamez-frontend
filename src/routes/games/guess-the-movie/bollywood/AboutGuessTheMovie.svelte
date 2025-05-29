ac31adc6e9dcab63bbfe8041c5b078e01f904bd0
Abhay123450 <95590546+Abhay123450@users.noreply.github.com>
2025-05-29 11:48:27 +0530

    updated: about GTM Bollywood

--- src/routes/games/guess-the-movie/bollywood/AboutGuessTheMovie.svelte
+++ src/routes/games/guess-the-movie/bollywood/AboutGuessTheMovie.svelte
@@ -3,23 +3,30 @@
 	class="flex flex-col w-auto h-full p-2 rounded-md bg-white space-y-1 overflow-y-auto rounded-scrollbar"
 >
 	<header>
-		<h2 id="about-title" class="text-xl font-bold">About Guess The Movie - Bollywood</h2>
+		<h2 id="about-title" class="text-xl font-bold">About Guess The Movie – Bollywood</h2>
 	</header>
 
 	<p>
-		<strong>Guess The Movie - Bollywood</strong> is an exciting <em>movie guessing game</em> where players
-		must guess the name of a movie one letter at a time before running out of lives and time.
+		<strong>Guess The Movie – Bollywood</strong> is a fun and exciting <em>movie guessing game</em>
+		that you can play online for free on your mobile device, tablet, or computer at QuickGamez.com. In
+		this
+		<em>guess the movie game</em>, players must uncover the names of popular Hindi movies one letter
+		at a time before they run out of lives and time. With over 600 movie titles to guess,
+		categorized into three difficulty levels — Easy, Medium, and Hard — this
+		<em>guess the movie game</em>
+		offers hours of entertainment. Whether you're a movie buff or just looking for a fun way to pass
+		the time, play this free guess the movie online game today and see how many titles you can guess!
 	</p>
 
 	<section aria-labelledby="start-title">
 		<h3 id="start-title" class="text-xl font-bold">Starting the Game</h3>
 		<ul class="list-disc ps-4 lg:ps-5">
-			<li>Choose a difficulty level: Easy, Medium, or Hard.</li>
+			<li>Choose a difficulty level for the movie: Easy, Medium, or Hard.</li>
 			<li>
 				Enable or disable the game timer using the "Timer" toggle. <strong>Note:</strong> Games played
 				without the timer will not count toward the leaderboard.
 			</li>
-			<li>Switch keyboard layouts with the "QWERTY keyboard" toggle button.</li>
+			<li>Switch keyboard layouts using the "QWERTY keyboard" toggle button.</li>
 			<li>Enter fullscreen mode using the "Fullscreen" toggle button.</li>
 			<li>Click the "Start" button to begin the game.</li>
 		</ul>
@@ -32,11 +39,15 @@
 				You start the game with 5 lives and a limited amount of time to guess the movie title.
 			</li>
 			<li>
-				Guess one letter at a time. Incorrect guesses cost a life and reveal a hint. The guessed
-				letter is disabled for future use.
+				Initially, one of the five hints will be revealed. The remaining four hints will be revealed
+				after each incorrect guess.
+			</li>
+			<li>
+				You have to guess the movie title one letter at a time using the on-screen or physical
+				keyboard.
 			</li>
 			<li>Correct guesses reveal the first occurrence of the letter in the movie title.</li>
-			<li>You can use either your physical keyboard or the on-screen keyboard to play.</li>
+			<li>Incorrect guesses cost a life and reveal a hint. The guessed letter is then disabled.</li>
 			<li>If you guess the full movie title before running out of lives or time, you win!</li>
 			<li>If you lose all your lives or run out of time, the game ends in a loss.</li>
 		</ul>
@@ -44,8 +55,6 @@
 
 	<section aria-labelledby="leaderboard-title">
 		<h3 id="leaderboard-title" class="text-xl font-bold">Climbing the Leaderboard</h3>
-		<p></p>
-		<p></p>
 		<ul class="list-disc ps-4 lg:ps-5">
 			<li>
 				Your score determines your position on the daily, weekly, and all-time leaderboards. The
@@ -63,13 +72,17 @@
 		<h3 id="points-title" class="text-xl font-bold">Points System</h3>
 		<ul class="list-disc ps-4 lg:ps-5">
 			<li>+20 points for each remaining life.</li>
-			<li>+1 point for every second remaining on the timer.</li>
 			<li>+100 points for winning in Easy difficulty.</li>
 			<li>+200 points for winning in Medium difficulty.</li>
 			<li>+300 points for winning in Hard difficulty.</li>
 			<li>
 				100 total points are distributed across all correct guesses. So, if there are 4 characters
 				to guess, each correct guess earns 25 points.
+			</li>
+			<li>
+				For time remaining, points are calculated based on the percentage of time left. For example,
+				if 100 seconds is the total time and you finish with 60 seconds remaining, you get 60
+				points.
 			</li>
 		</ul>
 	</section>
