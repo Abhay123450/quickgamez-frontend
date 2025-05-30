587e44cf820deaee48e3f84aa171271731062e25
Abhay123450 <95590546+Abhay123450@users.noreply.github.com>
2025-05-30 17:32:02 +0530

    return 410 status for php pages - added circleLogo

--- src/routes/+layout.ts
+++ src/routes/+layout.ts
@@ -1,2 +1,17 @@
+import { error } from '@sveltejs/kit';
+
 export const prerender = true;
 // export const trailingSlash = 'never';
+
+export function load({ url }) {
+	if (
+		url.pathname.toLowerCase().includes('.php') ||
+		url.pathname.includes('.php7') ||
+		url.pathname.includes('wp-admin') ||
+		url.pathname.includes('wp-includes') ||
+		url.pathname.includes('wp-content') ||
+		url.pathname.includes('ads.txt')
+	) {
+		error(410, 'Gone');
+	}
+}
